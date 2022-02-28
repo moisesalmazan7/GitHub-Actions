f96af4e0bb20f41cabe2cc1a8c9f8e9268464a85
moisesalmazan7 <63236552+moisesalmazan7@users.noreply.github.com>
2022-02-28 17:36:21 +0100

    Create test_multiplicacion.py

---
+++ tests/test_multiplicacion.py
@@ -0,0 +1,9 @@
+from app.operaciones import multiplicacion
+
+
+class TestClass:
+    def test_multiplicacion(self):
+        assert suma(4, 5) == 20
+        assert suma(1, 2) == 2
+        assert suma(7, 5) == 35
+        assert suma(7, 9) == 49
