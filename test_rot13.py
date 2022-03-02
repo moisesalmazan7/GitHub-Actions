04d5ab4eb6ccbb44cd8b0ce79bf189a318660918
moisesalmazan7 <63236552+moisesalmazan7@users.noreply.github.com>
2022-03-02 17:08:07 +0100

    Delete test_rot13.py

--- test_rot13.py
+++
@@ -1,6 +0,0 @@
-from app.rot13_cipher import rot13 
-
-class TestClass:
-    def test_rot13(self):
-        assert rot13("Me llamo Moisés") == "Zr yynzb Zbvféf"
-        assert rot13("Tengo 23 años") == "Gratb 23 nñbf"        
