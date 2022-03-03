3b7f54cc9ff3a3df3b96dbce43a4365b92bb31bf
Moises Almazan <moisesalmazan1999@gmail.com>
2022-03-03 19:51:28 +0100

    subiendo tests

--- tests/test_division.py
+++ tests/test_division.py
@@ -4,6 +4,6 @@ from app.operaciones import division
 class TestClass:
     def test_division(self):
         assert division(20, 5) == 4
-        assert division(8, 2) == -4
+        assert division(8, 2) == 4
         assert division(10, 5) == 2
         assert division(9, 9) == 1
--- tests/test_multiplicacion.py
+++ tests/test_multiplicacion.py
@@ -3,7 +3,7 @@ from app.operaciones import multiplicacion
 
 class TestClass:
     def test_multiplicacion(self):
-        assert suma(4, 5) == 20
-        assert suma(1, 2) == 2
-        assert suma(7, 5) == 35
-        assert suma(7, 9) == 49
+        assert multiplicacion(4, 5) == 20
+        assert multiplicacion(1, 2) == 2
+        assert multiplicacion(7, 5) == 35
+        assert multiplicacion(7, 9) == 63
